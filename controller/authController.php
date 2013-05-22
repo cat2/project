e3bd54b0d823c592ce25335e33447304d4ef5c6b
cramblda <david@functionalchaos.net>
2013-05-21 20:27:08 -0700

    Update session support

--- controller/authController.php
+++ controller/authController.php
@@ -1,12 +1,22 @@
 <?php
 
-// Include libraries
-include(APP_MODEL . "/auth/auth_lib.php");
-
 
 switch ( $_GET["a"] ) {
 
     case "login":
+        include(APP_VIEW . "/header.php");
+        include(APP_VIEW . "/nav.php");
+        include(APP_VIEW . "/auth/loginView.php" );
+        include(APP_VIEW . "/footer.php");
+        break;
+
+    case "logout":
+
+        # Delete Session Data
+        $_SESSION = 0;
+        session_destroy();
+        session_start();
+
         include(APP_VIEW . "/header.php");
         include(APP_VIEW . "/nav.php");
         include(APP_VIEW . "/auth/loginView.php" );
@@ -18,6 +28,11 @@ switch ( $_GET["a"] ) {
         $auth = processAuth( $_POST["username"], $_POST["password"] );
 
         if ( true == $auth["return"] ) {
+
+            # Setup Session
+            $_SESSION["username"] = $_POST["username"];
+
+            # Redirect to App home page
             header("Location: http://localhost" . APP_DOC_ROOT );
         }
         else {
