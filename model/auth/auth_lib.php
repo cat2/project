0a1b749c10aa2011cc918ce10ab22173cf2af5de
cramblda <david@functionalchaos.net>
2013-05-28 18:31:01 -0700

    update authentication

--- model/auth/auth_lib.php
+++ model/auth/auth_lib.php
@@ -2,21 +2,22 @@
 
 function processAuth( $user, $pass ) {
 
-    $userList = array();
-    $userList["david"] = "test1234";
-    $userList["admin"] = "admin1234";
-    $userList["user"]  = "user1234";
-
     $auth = array();
     $auth["return"]  = false;
     $auth["message"] = "";
 
+    $sql = "SELECT *
+            FROM auth_user
+            WHERE username = '$user'";
+
+    $res = mysql_query( $sql );
+    $row = mysql_fetch_array( $res );
+
     // Check if user exisits
-    $userArray = array_keys( $userList );
-    if ( in_array( $user, $userArray ) ) {
+    if ( $row ) {
 
         //Check if password is valid
-        if ( $pass == $userList[$user] ) {
+        if ( $row["password"] == md5($pass) ) {
             $auth["return"]  = true;
         }
         else {
